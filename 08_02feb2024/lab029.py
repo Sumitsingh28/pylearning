19990605e0db3d3207c093d3118e6d6bebbd0a3c
HP <sumitsingh0828@gmail.com>
2024-02-08 21:31:36 +0530

    hello

--- 08_02feb2024/lab029.py
+++ 08_02feb2024/lab029.py
@@ -1,1 +1,7 @@
-print("hello sumit")
+π = 3.14
+
+Radius = float (input ("Please enter the radius of the given circle: "))
+
+area_of_the_circle = π * Radius * Radius
+
+print (" The area of the given circle is: ", area_of_the_circle)
---
+++ 08_02feb2024/lab030.py
@@ -0,0 +1,4 @@
+num1 = int(input("enter the first number"))
+num2 = int(input("enter the second number"))
+num3 = num1 + num2
+print(num3)
